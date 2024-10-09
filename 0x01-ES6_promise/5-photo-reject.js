65c1a10bcd9cf9a36180e61ebff12edc22ec0e1b
Ademitidayo <adenijibimmy08@gmail.com>
2024-10-09 13:25:53 +0100

    promise lint pass

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,3 @@
 export default function uploadPhoto(filename) {
-	rt default function uploadPhoto(filename) {
-		  return Promise.reject(Error(`${filename} cannot be processed`));
-	}
   return Promise.reject(Error(`${filename} cannot be processed`));
 }
